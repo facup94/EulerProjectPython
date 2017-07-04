4bf4692a75bcba6a0c20fbacd0625d08fe057e55
Facu Parra <facundo.parra.94@gmail.com>
2017-07-04 13:08:24 -0300

    Problem 18

---
+++ Scripts/18c.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+import time
+
+entrada = "75\n\
+95 64\n\
+17 47 82\n\
+18 35 87 10\n\
+20 04 82 47 65\n\
+19 01 23 75 03 34\n\
+88 02 77 73 07 63 67\n\
+99 65 04 28 06 16 70 92\n\
+41 41 26 56 83 40 80 70 33\n\
+41 48 72 33 47 32 37 16 94 29\n\
+53 71 44 65 25 43 91 52 97 51 14\n\
+70 11 33 28 77 73 17 78 39 68 17 57\n\
+91 71 52 38 17 14 91 43 58 50 27 29 48\n\
+63 66 04 68 89 53 67 30 73 16 69 87 40 31\n\
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"
+
+def imprimirMatriz(mat):
+    cadena = ""
+    for fila in mat:
+        for elem in fila:
+            cadena += str(elem) + " "
+        cadena += "\n"
+    print(cadena)
+
+def main():
+    a = entrada.split("\n")
+    a = [b.split(" ") for b in a]
+    matriz = []
+    for b in a:
+        l = []
+        for item in b:
+            l.append(int(item))
+        matriz.append(l)
+    
+
+    for fila in range(len(matriz)-2, -1, -1):
+        for elemento in range(len(matriz[fila])):
+            matriz[fila][elemento] += max(matriz[fila+1][elemento] , matriz[fila+1][elemento+1])
+
+    print(matriz[0][0])
+    
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
+    
