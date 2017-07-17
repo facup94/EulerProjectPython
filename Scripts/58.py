0c5bf1041efa5fd0f38d097d29ff1d08a5d39016
Facu Parra <facundo.parra.94@gmail.com>
2017-07-16 23:18:21 -0300

    Problem 58

---
+++ Scripts/58.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import time
+from math import sqrt
+
+def is_prime(x):
+    if x == 1: return False
+    if x == 2: return True
+    if x%2==0: return False
+    if x < 9: return True
+    if x%3==0: return False
+    
+    sqrtx = int(sqrt(x))
+    i = 5
+    while i <= sqrtx:
+        if x % i == 0: return False
+        if x % (i+2) == 0: return False
+        i=i+6
+
+    return True
+
+def main():
+    ratio = 1
+    dist = 2
+    last_added = 1
+    diagonal_size = 1
+    prime_amount = 0
+    while ratio > 0.1:
+        for _ in range(4):
+            last_added += dist
+            diagonal_size += 1
+            if is_prime(last_added): prime_amount += 1
+        dist += 2
+        ratio = prime_amount/diagonal_size
+
+    print(dist - 1)
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
